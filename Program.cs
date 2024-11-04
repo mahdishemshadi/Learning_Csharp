0352c11cf5434982c787817a7d89f3c3229d716a
Mehdishemshadi <mahdi.shemshadi@gmail.com>
2024-11-04 11:09:55 +0330

    Add variables and print them

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int count = 10;
+byte number = 25;
+char character = 'A';
+bool isWorking = true;
+float totalPrice = 20.95f;
+string firstName = "Madhi";
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine(number);
+Console.WriteLine(count);
+Console.WriteLine(character);
+Console.WriteLine(isWorking);
+Console.WriteLine(totalPrice);
+Console.WriteLine(firstName);
