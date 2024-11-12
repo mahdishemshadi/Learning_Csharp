7139cf10562614e167988259f0d05704188adfd7
Mehdishemshadi <mahdi.shemshadi@gmail.com>
2024-11-12 09:57:32 +0330

    Add some prints

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ string firstName = "Madhi";
 const string fullName = "Mahdi Shemshadi";
 
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Working with variables");
 Console.WriteLine(number);
 Console.WriteLine(count);
 Console.WriteLine(character);
@@ -14,6 +16,9 @@ Console.WriteLine(isWorking);
 Console.WriteLine(totalPrice);
 Console.WriteLine(firstName);
 
+Console.WriteLine("Working with format string");
 Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
 Console.WriteLine("{0} {1}", double.MinValue, double.MaxValue);
+
+Console.WriteLine("Working with const variables");
 Console.WriteLine("My name is {0}", fullName);
